e51c52831f8b71da9b9f72daa000af5ddb3f1a17
Zoe <s1200037@student.windesheim.nl>
2025-09-12 10:36:34 +0200

    Variabelen gewijzigd in properties. In de constructor waarden doorgegeven/gekoppeld aan de properties.

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,12 +2,14 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
+        public int GroceryListId { get; set; } 
+        public int ProductId { get; set; }     
         public int Amount { get; set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            //To do:assign values to the properties
+            GroceryListId = groceryListId; 
+            ProductId = productId;
+            Amount = amount;
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
