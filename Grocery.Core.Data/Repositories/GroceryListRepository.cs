4c801b6192884e7efed544438d3e8f5bb1600015
Zoe <s1200037@student.windesheim.nl>
2025-09-12 10:48:13 +0200

    naam veranderd boodschappenlijst (test).

--- Grocery.Core.Data/Repositories/GroceryListRepository.cs
+++ Grocery.Core.Data/Repositories/GroceryListRepository.cs
@@ -10,7 +10,7 @@ namespace Grocery.Core.Data.Repositories
         public GroceryListRepository()
         {
             groceryLists = [
-                new GroceryList(1, "Boodschappen familieweekend", DateOnly.Parse("2024-12-14"), "#FF6A00", 1),
+                new GroceryList(1, "Familieweekend boodschappen", DateOnly.Parse("2024-12-14"), "#FF6A00", 1),
                 new GroceryList(2, "Kerstboodschappen", DateOnly.Parse("2024-12-07"), "#626262", 1),
                 new GroceryList(3, "Weekend boodschappen", DateOnly.Parse("2024-11-30"), "#003300", 1)];
         }
